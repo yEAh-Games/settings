5367edbe9ee2543631b255d0a2bac7bd2fca0474
Tallinn <taq@unigon.net>
2023-07-12 00:39:51 -0400

    Test

--- scripts/profile.js
+++ scripts/profile.js
@@ -47,6 +47,8 @@ document.addEventListener("DOMContentLoaded", function () {
   
                 var testNameElement = document.getElementById("profileName");
                 testNameElement.value = userProfile.name;
+                var largeUsername = document.getElementById("largeUsername");
+                largeUsername.value = userData.username;
               })
               .catch(function (error) {
                 console.error("Error:", error);
@@ -61,7 +63,7 @@ document.addEventListener("DOMContentLoaded", function () {
   
       var adminsvg =
         '<a href="https://admin.yeahgames.net" style="text-decoration:none!important;" title="This user is a yEAh admin."><svg style="color:#ff4747!important;" class="VQS2tmQ_zFyBOC2tkmto YIUegm7fh_CpJbivTu6B MnxxlQlR1H0xJuMEE8Yr PeR2JZ9BZHYIH8Ea3F36 bcsWqjK52oeyT6oeC2Az gZ3KuFw1JESHhOJhjT8j _Oyukq8JlN1X9w2FmPds XIIs8ZOri3wm8Wnj9N_y Lld6j9B1iilEqA6j31e4" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M5 9V7a5 5 0 0110 0v2a2 2 0 012 2v5a2 2 0 01-2 2H5a2 2 0 01-2-2v-5a2 2 0 012-2zm8-2v2H7V7a3 3 0 016 0z" clip-rule="evenodd"></path></svg></a>';
-  
+      
       function insertAdminHTML(userProfile) {
         var profileAdminElement = document.getElementById("profileAdmin");
         if (userProfile.isAdmin) {
