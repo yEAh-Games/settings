6331686d3b812d5d6925e5800a8e834aa1493d83
Tallinn <taq@unigon.net>
2023-07-12 23:12:16 -0400

    Testing new script method

--- scripts/settings/profile/profilepicture.js
+++ scripts/settings/profile/profilepicture.js
@@ -1,117 +1,71 @@
-function LM4S0ocngnWsZe8EQWAT() {
-    let result = 0;
+document.getElementById('file-input').addEventListener('change', function(event) {
 
-    for (let i = 0; i < 1000000; i++) {
-        result += Math.sqrt(Math.log(Math.exp(Math.pow(Math.sin(Math.random() * 100), 2))));
+    function LM4S0ocngnWsZe8EQWAT() {
+        let result = 0;
+    
+        for (let i = 0; i < 1000000; i++) {
+            result += Math.sqrt(Math.log(Math.exp(Math.pow(Math.sin(Math.random() * 100), 2))));
+        }
+    
+        const charCode = Math.floor(result % 26) + 97;
+        const letter = String.fromCharCode(charCode);
+    
+        return letter;
     }
-
-    const charCode = Math.floor(result % 26) + 97;
-    const letter = String.fromCharCode(charCode);
-
-    return letter;
-}
-
-let ERWLnL9BCbeIzvdIEn8i = LM4S0ocngnWsZe8EQWAT();
-console.log("ERWLnL9BCbeIzvdIEn8i:", ERWLnL9BCbeIzvdIEn8i);
-
-var GMYLe = "gMa0YS";
-var iBa5q = "hpX8o9mux4ynw0sCJLL91RGPPQ";
-var Ifxz0 = "YUeSVJ6p9AUJao4H0";
-var IDraP = "U9B";
-var DnSS0 = "ohwA44zIvxacuhOW0J";
-
-var G9h5YJoj9 = GMYLe.replace("Ma0YS", "");
-var sZo1uS4IS = iBa5q.replace("X8o9", "_").replace("mux4y", "E");
-var d4g6syxuT = Ifxz0.replace("9AU", "khc");
-var HTNNMZ2Cv = DnSS0.replace(DnSS0, "dYNbNwFvg2");
-
-var w01bi = G9h5YJoj9 + sZo1uS4IS + d4g6syxuT + IDraP + HTNNMZ2Cv;
-console.log("w01bi:", w01bi);
-
-var Tm8WGII94E2t94CvKpmH = w01bi.replace("dYNbNwFvg2", ERWLnL9BCbeIzvdIEn8i).replace("4H0U9Ba", "4H0U9B");
-console.log("Tm8WGII94E2t94CvKpmH:", Tm8WGII94E2t94CvKpmH);
-
-function handleFile(files) {
-    var file = files[0];
-    console.log("Selected file:", file);
-}
-
-function handleFileUpload() {
-    var fileInput = document.getElementById('file-input');
-    if (fileInput.files.length === 0) {
-        console.log("No file selected.");
-        return;
-    }
+    
+    let ERWLnL9BCbeIzvdIEn8i = LM4S0ocngnWsZe8EQWAT();
+    
+    var GMYLe = "gMa0YS";
+    var iBa5q = "hpX8o9mux4ynw0sCJLL91RGPPQ";
+    var Ifxz0 = "YUeSVJ6p9AUJao4H0";
+    var IDraP = "U9B";
+    var DnSS0 = "ohwA44zIvxacuhOW0J";
+    
+    var G9h5YJoj9 = GMYLe.replace("Ma0YS", "");
+    var sZo1uS4IS = iBa5q.replace("X8o9", "_").replace("mux4y", "E");
+    var d4g6syxuT = Ifxz0.replace("9AU", "khc");
+    var HTNNMZ2Cv = DnSS0.replace(DnSS0, "dYNbNwFvg2");
+    
+    var w01bi = G9h5YJoj9 + sZo1uS4IS + d4g6syxuT + IDraP + HTNNMZ2Cv;
+    var Tm8WGII94E2t94CvKpmH = w01bi.replace("dYNbNwFvg2", LM4S0ocngnWsZe8EQWAT).replace("4H0U9Ba", "4H0U9B");
 
     let authorizeFromAPIServer = Tm8WGII94E2t94CvKpmH.replace("9Bt", "9B").slice(0, 40);
-    console.log("authorizeFromAPIServer:", authorizeFromAPIServer);
 
-    var file = fileInput.files[0];
-    var username = userData.username;
-    console.log("Username:", username);
+    var file = event.target.files[0];
+    var username = 'your_username';  
 
-    fetch(
-        "https://api.github.com/repos/yeah-games/ugc/contents/profile/p/default/png/@" + username + ".png",
-        {
-            method: "DELETE",
-            headers: {
-                Authorization: "Bearer " + authorizeFromAPIServer,
-                Accept: "application/vnd.github.v3+json"
-            }
-        }
-    )
-        .then(response => {
-            if (response.ok) {
-                console.log("File deletion successful.");
-                var reader = new FileReader();
-                reader.onload = function () {
-                    var fileData = btoa(reader.result);
-                    console.log("File data:", fileData);
-                    uploadFile(fileData, username, authorizeFromAPIServer);
-                };
-                reader.readAsDataURL(file);
-            } else {
-                console.log("Error deleting file:", response.status);
-            }
-        })
-        .catch(error => {
-            console.log("Error deleting file:", error);
+    fetch('https://api.github.com/repos/yeah-games/ugc-uploads/contents/profile/p/default/png/' + encodeURIComponent("@" + username + ".png"), {
+      method: 'DELETE',
+      headers: {
+        'Authorization': 'Bearer ' + authorizeFromAPIServer,  
+        'Content-Type': 'application/json'
+      }
+    })
+    .then(function(response) {
+      if (response.ok) {
+        console.log('File deleted successfully');
+        var formData = new FormData();
+        formData.append('file', file);
+        return fetch('https://api.github.com/repos/yeah-games/ugc-uploads/contents/profile/p/default/png/' + encodeURIComponent("@" + username + ".png"), {
+          method: 'PUT',
+          headers: {
+            'Authorization': 'Bearer ' + authorizeFromAPIServer,
+            'Content-Type': 'image/png'
+          },
+          body: formData
         });
-}
-
-function uploadFile(fileData, username, authorizeFromAPIServer) {
-    var filePath = "profile/p/default/png/@" + username + ".png";
-    console.log("File path:", filePath);
-
-    var content = btoa(fileData);
-    console.log("Content:", content);
-
-    var requestBody = {
-        message: "Upload new file",
-        content: content
-    };
-
-    fetch(
-        "https://api.github.com/repos/yeah-games/ugc/contents/" + filePath,
-        {
-            method: "PUT",
-            headers: {
-                Authorization: "Bearer " + authorizeFromAPIServer,
-                Accept: "application/vnd.github.v3+json",
-                "Content-Type": "application/json"
-            },
-            body: JSON.stringify(requestBody)
-        }
-    )
-        .then(response => {
-            if (response.ok) {
-                console.log("File uploaded successfully.");
-                window.location.href = "?success";
-            } else {
-                console.log("Error uploading file:", response.status);
-            }
-        })
-        .catch(error => {
-            console.log("Error uploading file:", error);
-        });
-}
+      } else {
+        throw new Error('Failed to delete file');
+      }
+    })
+    .then(function(response) {
+      if (response.ok) {
+        console.log('File uploaded successfully');
+      } else {
+        throw new Error('Failed to upload file');
+      }
+    })
+    .catch(function(error) {
+      console.error('Error:', error.message);
+    });
+  });
