f608fe0e39fbdae42f366b85876a5a32d4ead8e6
Tallinn <taq@unigon.net>
2023-10-07 23:04:14 -0400

    Updates + new settings

--- scripts/settings/profile/load.js
+++ scripts/settings/profile/load.js
@@ -6,7 +6,7 @@ document.addEventListener("DOMContentLoaded", function () {
     if (userData && userData.username) {
         var authorizedUsername = userData.username;
 
-        fetch("https://accounts.yeahgames.net/data/settings/profiles.json")
+        fetch("https://accounts.ynwk.org/data/settings/profiles.json")
             .then(function (response) {
                 if (response.ok) {
                     return response.json();
@@ -20,7 +20,7 @@ document.addEventListener("DOMContentLoaded", function () {
                 });
 
                 if (userProfile) {
-                    fetch("https://accounts.yeahgames.net/data/accounts.json")
+                    fetch("https://accounts.ynwk.org/data/accounts.json")
                         .then(function (response) {
                             if (response.ok) {
                                 return response.json();
@@ -56,7 +56,7 @@ document.addEventListener("DOMContentLoaded", function () {
                             }
                            
                             var profilePictureArea = document.getElementById("profilePicture");
-                            var profilePictureSrc = "https://ugc.yeahgames.net/profile/p/default/png/" + "@" + userData.username + ".png";
+                            var profilePictureSrc = "https://ugc.ynwk.org/profile/p/default/png/" + "@" + userData.username + ".png";
  
                             profilePictureArea.setAttribute("src", profilePictureSrc);
                         })
@@ -72,7 +72,7 @@ document.addEventListener("DOMContentLoaded", function () {
             });
 
         var adminsvg =
-            '<a href="https://admin.yeahgames.net" style="text-decoration:none!important;" title="This user is a yEAh admin."><svg style="color:#ff4747!important;" class="VQS2tmQ_zFyBOC2tkmto YIUegm7fh_CpJbivTu6B MnxxlQlR1H0xJuMEE8Yr PeR2JZ9BZHYIH8Ea3F36 bcsWqjK52oeyT6oeC2Az gZ3KuFw1JESHhOJhjT8j _Oyukq8JlN1X9w2FmPds XIIs8ZOri3wm8Wnj9N_y Lld6j9B1iilEqA6j31e4" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M5 9V7a5 5 0 0110 0v2a2 2 0 012 2v5a2 2 0 01-2 2H5a2 2 0 01-2-2v-5a2 2 0 012-2zm8-2v2H7V7a3 3 0 016 0z" clip-rule="evenodd"></path></svg></a>';
+            '<a href="https://admin.ynwk.org" style="text-decoration:none!important;" title="This user is a yEAh admin."><svg style="color:#ff4747!important;" class="VQS2tmQ_zFyBOC2tkmto YIUegm7fh_CpJbivTu6B MnxxlQlR1H0xJuMEE8Yr PeR2JZ9BZHYIH8Ea3F36 bcsWqjK52oeyT6oeC2Az gZ3KuFw1JESHhOJhjT8j _Oyukq8JlN1X9w2FmPds XIIs8ZOri3wm8Wnj9N_y Lld6j9B1iilEqA6j31e4" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M5 9V7a5 5 0 0110 0v2a2 2 0 012 2v5a2 2 0 01-2 2H5a2 2 0 01-2-2v-5a2 2 0 012-2zm8-2v2H7V7a3 3 0 016 0z" clip-rule="evenodd"></path></svg></a>';
 
         function insertAdminHTML(userProfile) {
             var profileAdminElement = document.getElementById("profileAdmin");
